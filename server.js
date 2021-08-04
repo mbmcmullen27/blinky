495157c302d9b7776a361ae1c77e43a0d0e4c8d9
fishbot <mbmcmullen27@gmail.com>
2021-08-04 07:02:56 -0500

    express router

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var express = require('express')
+var express = require('express');
+var router = express.Router();
 var fs = require('fs');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const prefix = '/app'
+
 var hits = 0;
 var app = express();
 
-app.use(express.static('public'))
+app.use(prefix,router);
 
-app.get('/blinky',(req,res)=>{
+router.use(express.static('public'))
+
+router.get('/blinky',(req,res)=>{
     console.log(`/blinky has been hit ${++hits} time/s`);
     const data = fs.readFileSync("blinky");
     res.send(data);
 })
 
-app.get('/hits', (req,res) => {
+router.get('/hits', (req,res) => {
     res.send(`${hits}\n`);
 })
 
