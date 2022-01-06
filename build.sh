6d82e01b0ebd2e180f5f995739a3c4aa9b2eb25a
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-01-05 20:45:58 -0600

    build and push workflow

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-if [ ! -z $1 ]; then
+tag=$1
+if [ ! -z $tag ]; then
     git pull
     docker build . -t mcmull27/blinky
-    docker tag mcmull27/blinky mcmull27/blinky:$1
-    docker push mcmull27/blinky:$1
+    docker tag mcmull27/blinky mcmull27/blinky:$tag
+    docker push mcmull27/blinky:$tag
+else
+    echo -e "->> Missing image tag for docker push \nusage: \n  ./build.sh [image tag]"
 fi
